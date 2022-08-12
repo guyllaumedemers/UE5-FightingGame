c5731d677250f9af581a32ba7b53f468c23ee451
guyllaumedemers <guyllaume_demers@hotmail.fr>
2022-08-12 07:59:33 -0400

    pushing debugging test before going to in office, enumValue return is always Invalid. to investigate

--- Source/FightingGame/InputActionTableRow.cpp
+++ Source/FightingGame/InputActionTableRow.cpp
@@ -3,20 +3,21 @@
 
 #include "InputActionTableRow.h"
 
+#include "Engine/Engine.h"
+
 //@TODO Improve the following prototype for better flexibility
 //		and cover additional edge cases.
 //
 FInputKey FInputKey::GetFInputKeyFromAction(const UInputAction* InputAction)
 {
 	FString InputActionValue = InputAction->GetFName().ToString();
-	static const enum class EActionType
+	enum class EActionType
 	{
 		None = 0,
 		Action,
 		Direction
-	};
+	} InputActionType = EActionType::None;
 
-	EActionType InputActionType = EActionType::None;
 	int SIndex = 0;
 	while (InputActionValue.FindChar('_', SIndex))
 	{
@@ -24,14 +25,16 @@ FInputKey FInputKey::GetFInputKeyFromAction(const UInputAction* InputAction)
 		// TODO Always Return ActionType::None, fix it
 		//		odd...		
 		//
+		UE_LOG(LogTemp, Error, TEXT("%s"), *SubStr);
 		InputActionType = FEnumTool::GetEnumValueFromString<EActionType>("EActionType", SubStr);
+		UE_LOG(LogTemp, Error, TEXT("%s"), *FEnumTool::GetEnumValueAsString<EActionType>("EActionType", InputActionType));
 		InputActionValue.RemoveFromStart(SubStr + TEXT("_"));
 	}
 	return [&](const EActionType& ActionType, const FString& StringValue)
 	{
 		// this is exactly what I wanted to avoid, if we are to add Shoulder Trigger, Joystick triggers, etc not only
 		// do we have to extend the enum but also this switch case. Every Header/.cpp file referencing this header will then have to run the pre-processor on this @include to update
-		// the translation unit for this .cpp with the updated code and re-compile all translation unit referencing it... bad physical design.
+		// their translation unit with the updated enumeration and re-compile... bad physical design.
 		//
 		switch (ActionType)
 		{
--- Source/FightingGame/InputActionTableRow.h
+++ Source/FightingGame/InputActionTableRow.h
@@ -12,11 +12,20 @@ struct FEnumTool
 {
 	template <typename EnumType>
 	static FORCEINLINE EnumType GetEnumValueFromString(const FString& EnumName, const FString& String) {
-		UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, *EnumName, true);
+		const UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, *EnumName, true);
 		if (!Enum) {
 			return EnumType(0);
 		}
 		return (EnumType)Enum->GetIndexByName(FName(*String));
+	}
+
+	template<typename TEnum>
+	static FORCEINLINE FString GetEnumValueAsString(const FString& Name, TEnum Value) {
+		const UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, *Name, true);
+		if (!EnumPtr) {
+			return FString("Invalid");
+		}
+		return EnumPtr->GetNameByValue((int64)Value).ToString();
 	}
 };
 

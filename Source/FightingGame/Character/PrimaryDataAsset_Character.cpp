e9023ebcc3a074eca9ba17d421587e8a5787dafc
guyllaumedemers <guyllaume_demers@hotmail.fr>
2022-09-19 09:48:34 -0400

    deleting project. currently working on wireframing this thing properly on pen and paper before doing more dumb shit.

--- Source/FightingGame/Character/PrimaryDataAsset_Character.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PrimaryDataAsset_Character.h"
-
-FPrimaryAssetId UPrimaryDataAsset_Character::GetPrimaryAssetId() const
-{
-	return Super::GetPrimaryAssetId();
-}
